54ca8353e11911e66f2c28e54bb0ab871eb60478
th-release <th.release.cth@gmail.com>
2022-09-23 18:45:19 +0900

    portfolio

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,22 @@ import Head from 'next/head'
 const Home: NextPage = () => {
   return (
     <main className='main'>
-      <div className='sidebar'>
-        <a style={{ fontSize: "45px" }}>T<br/>H<br/>R<br/>E<br/>L<br/>E<br/>A<br/>S<br/>E<br/>C<br/>T<br/>H</a>
+      <div className='LeftSidebar'>
+        <a style={{ fontSize: "215%", fontWeight: 400 }}>T<br/>H<br/>|<br/>R<br/>E<br/>L<br/>E<br/>A<br/>S<br/>E<br/>|<br/>C<br/>T<br/>H</a>
+      </div>
+      <div className='RightSidebar'>
+        <a style={{ fontSize: "215%", fontWeight: 400 }}>P<br/>O<br/>R<br/>T<br/>F<br/>O<br/>L<br/>I<br/>O</a>
       </div>
       <article className="first">
         <h1>환영합니다, unknown</h1>
         <p>스크롤을 내려주세요.</p>
       </article>
-      <article className="second">
-        
+      <article className="first">
+        <h1>환영합니다, unknown</h1>
+        <p>스크롤을 내려주세요.</p>
+      </article>
+      <article className='second'>
+
       </article>
     </main>
   )
